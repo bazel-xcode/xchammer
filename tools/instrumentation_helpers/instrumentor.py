9563781a2d99ae76961eb5a000265b3825d7f19b
Jerry Marino <i@jerrymarino.com>
2018-12-07 13:29:48 -0800

    Create instrumentor lib

---
+++ tools/instrumentation_helpers/instrumentor.py
@@ -0,0 +1,48 @@
+import os
+import time
+import socket
+import json
+
+TSD_HOST = ""
+TSD_PORT = 80
+
+TSD_TIMEOUT_SEC = 5
+
+def write_tsd(metric, delta):
+    timestamp = int(round(time.time()))
+    tags = "host={}".format(socket.gethostname())
+    tsd = "put {metric} {timestamp} {delta} {tags}\n".format(metric=metric,
+                                                           timestamp=timestamp,
+                                                           delta=delta,
+                                                           tags=tags)
+    try:
+        sock = socket.create_connection((TSD_HOST, TSD_PORT),
+                                        timeout=TSD_TIMEOUT_SEC)
+        sock.sendall(tsd)
+        sock.close()
+    except Exception:
+        print("failed to write '{}' to {}:{}".format(tsd, TSD_HOST, TSD_PORT))
+
+
+def write_build_metric():
+    start_time_f = os.path.join(
+        os.environ.get('TARGET_BUILD_DIR'),'xchammer.build_start')
+    start_time = os.path.getmtime(start_time_f)
+    delta = ((time.time()-start_time)*1000)
+    write_tsd("xchammer.build", delta)
+
+
+def write_last_generation_metric():
+    build_dir = os.environ.get('OBJROOT')
+    if build_dir:
+        base_path = build_dir
+    else:
+        base_path = "/private/var/tmp"
+
+    last_generation_log = os.path.join(base_path, 'xchammer.log')
+    with open(last_generation_log, "r") as f:
+        for l, i in enumerate(f):
+            json_str = i.split("\n")[0]
+            trace_entry = json.loads(json_str)
+            write_tsd(trace_entry["name"], trace_entry["ts"])
+
--- tools/instrumentation_helpers/statsd_post_generate_action.py
+++ tools/instrumentation_helpers/statsd_post_generate_action.py
@@ -1,6 +1,4 @@
 import instrumentor
-
 # Write the build metric and then write the last generation metric
-instrumentor.write_build_metric()
 instrumentor.write_last_generation_metric()
 
