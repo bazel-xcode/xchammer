93a82d7fa9be4c21b94a20fea4d6c9c83dd61e61
Jerry Marino <i@jerrymarino.com>
2019-08-14 12:30:50 -0700

    Use templating

--- tools/XCConfigExporter/main.swift
+++ tools/XCConfigExporter/main.swift
@@ -19,18 +19,26 @@ do {
     }
     let xcconfig = CommandLine.arguments[1]
 
+    // Create a temp directory for the template project, then copy it over
+    let attributes: [FileAttributeKey: Any] = [FileAttributeKey.posixPermissions: 0o777]
     let tmpDir = NSTemporaryDirectory() + "/" + UUID().uuidString
     try FileManager.default.createDirectory(atPath: tmpDir,
         withIntermediateDirectories: true,
-        attributes: [:])
-    try FileManager.default.copyItem(atPath: fixtureBundle,
-        toPath: tmpDir + "/Fixtures")
-    try FileManager.default.copyItem(atPath: xcconfig,
-        toPath: tmpDir + "/Config.xcconfig")
-
+        attributes: attributes)
+    try FileManager.default.copyItem(atPath: fixtureBundle, toPath: tmpDir + "/Fixtures")
 
     let iOSDir = tmpDir + "/Fixtures/iOSApp"
-    let buildCommand = ["/bin/bash -c 'xcodebuild -project iOSApp.xcodeproj -scheme iOSApp -sdk iphonesimulator12.2 -configuration Debug 2>&1'"]
+    let xcodeProjOutPath =  iOSDir + "/iOSApp.xcodeproj/project.pbxproj"
+    let xcodeProjTemplatePath =  fixtureBundle + "/iOSApp/iOSApp.xcodeproj/project.pbxproj"
+    let templateXcodeProj = try String(contentsOf: URL(fileURLWithPath:
+        xcodeProjTemplatePath))
+        .replacingOccurrences(of:"__CONFIG_ABSOLUTE_PATH__", with: xcconfig)
+    guard FileManager.default.createFile(atPath: xcodeProjOutPath,
+                contents: templateXcodeProj.data(using: .utf8), attributes:
+                attributes) else {
+             fatalError("Can't write xcode project")
+    }
+    let buildCommand = ["/bin/bash -c 'xcodebuild -project iOSApp.xcodeproj -scheme iOSApp -sdk iphonesimulator -configuration Debug 2>&1'"]
     let log = try ShellOut.shellOut(to: buildCommand, at: iOSDir)
     let parsed = parse(log: log)
     let diagFlags = parsed.compactMap {
